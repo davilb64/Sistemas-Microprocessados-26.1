3ef123555ac3ba747d47caf68fe80623ffe60b33
davilb64 <davilopesbrito64@gmail.com>
2026-04-04 22:14:34 -0300

    exp 2 enigma

--- enigma2/enigma2.asm
+++ enigma2/enigma2.asm
@@ -9,29 +9,48 @@ main:
 
   mov.w #MSG, R4 ;end. msg
   mov.w #GSM, R5 ;end. cifrada
+  call #ENIGMA2
 
-  call #ENIGMA1
-  
+  mov.w #GSM, R4 ;end. cifrada
+  mov.w #DCF, R5 ;end. decifrada
+
+  call #ENIGMA2
+
   jmp $
   nop
 
-ENIGMA1:
+ENIGMA2:
+  mov.w #0x0041, R7 ;Letra A
+  mov.w #RT1, R8 ;ponteiro RT1
+  mov.w #RF1, R9 ;ponteiro RF1
 
-  mov.w #RT1, R6 ;end. chave inicial
-  mov.b #0x41, R7
+  mov.b 0(R4), R13
+  sub.b R7, R13 ;tira 41 da letra atual
+  mov.b R13, R11
+  add.w R11, R8 ;move o ponteiro de rt1 pra desejada
+  mov.b 0(R8), R12
+  add.w R12, R9 ;move o ponteiro de RF1 pra desejada
+  mov.b 0(R9), R10 ;guarda valor que queremos alcançar
 
-  sub.b R7, 0(R4)
-  mov.b 0(R4), R8
-  add.w R8, R6
-  add.b 0(R6), R7
+  ;loop comparação
+  mov.w #RT1, R8 ;ponteiro RT1
+loopcomparacao:
+  cmp.b 0(R8),R10
+  jz igual
+  inc.w R8
+  jmp loopcomparacao
 
-  mov.b R7, 0(R5)
+igual:
+  mov.w #RT1, R10 ;inicio RT1
+  sub.w R10, R8
+  add.w R7, R8
+  mov.b R8, 0(R5)
 
-  inc.w(R5)
   inc.w(R4)
-  tst.b 0(R4)
-  jnz ENIGMA1
+  inc.w(R5)
 
+  tst.b 0(R4)
+  jnz ENIGMA2
 
   ret
 
@@ -40,7 +59,9 @@ ENIGMA1:
   .data
 MSG: .byte "CABECAFEFACAFAD",0 ;Mensagem em claro
 GSM: .byte "XXXXXXXXXXXXXXX",0 ;Mensagem cifrada
+DCF: .byte "XXXXXXXXXXXXXXX",0 ;Mensagem decifrada
 RT1: .byte 2, 4, 1, 5, 3, 0    ;Trama do Rotor 1
+RF1: .byte 3, 5, 4, 0, 2, 1    ;Tabela do Refletor
 
 
 
